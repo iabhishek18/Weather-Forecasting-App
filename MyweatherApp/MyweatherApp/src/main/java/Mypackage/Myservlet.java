3642b205a4897be6cc0dad1891878ff15ff804e5
ABHISHEK MISHRA <118846265+iabhishek18@users.noreply.github.com>
2024-07-01 11:12:03 +0530

    Add files via upload

---
+++ MyweatherApp/MyweatherApp/src/main/java/Mypackage/Myservlet.java
@@ -0,0 +1,104 @@
+package Mypackage;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Date;
+import java.util.Scanner;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class Myservlet
+ */
+public class Myservlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Myservlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String inputData=request.getParameter("userInput");
+//      Api setup
+		String apiKey = "b2f96e816a8d6d9315a7cf93d407eabc";
+		// Get the city from the form input
+        String city = request.getParameter("city"); 
+
+        // Create the URL for the OpenWeatherMap API request
+        String apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+        // Api integration
+        URL url = new URL(apiUrl);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        // Reading the data from network
+        InputStream inputStream = connection.getInputStream();
+        InputStreamReader reader = new InputStreamReader(inputStream);
+        // want to store in the string
+        Scanner scanner = new Scanner(reader);
+        StringBuilder responseContent = new StringBuilder();
+        while (scanner.hasNext()) {
+            responseContent.append(scanner.nextLine());
+        }
+        scanner.close();
+        System.out.println(responseContent);
+        // Parsing the data into json
+        Gson gson = new Gson();
+        JsonObject jsonObject = gson.fromJson(responseContent.toString(), JsonObject.class);
+      //Date & Time
+        long dateTimestamp = jsonObject.get("dt").getAsLong() * 1000;
+        String date = new Date(dateTimestamp).toString();
+        
+        //Temperature
+        double temperatureKelvin = jsonObject.getAsJsonObject("main").get("temp").getAsDouble();
+        int temperatureCelsius = (int) (temperatureKelvin - 273.15);
+       
+        //Humidity
+        int humidity = jsonObject.getAsJsonObject("main").get("humidity").getAsInt();
+        
+        //Wind Speed
+        double windSpeed = jsonObject.getAsJsonObject("wind").get("speed").getAsDouble();
+        
+        //Weather Condition
+        String weatherCondition = jsonObject.getAsJsonArray("weather").get(0).getAsJsonObject().get("main").getAsString();
+        // Set the data as request attributes (for sending to the jsp page)
+        request.setAttribute("date", date);
+        request.setAttribute("city", city);
+        request.setAttribute("temperature", temperatureCelsius);
+        request.setAttribute("weatherCondition", weatherCondition); 
+        request.setAttribute("humidity", humidity);    
+        request.setAttribute("windSpeed", windSpeed);
+        request.setAttribute("weatherData", responseContent.toString());
+        connection.disconnect();
+
+        // Forward the request to the weather.jsp page for rendering
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+        
+
+	}
+
+}
